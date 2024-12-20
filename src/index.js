e513631090e2a8b10864788de004d8cd33b7be52
ARYAN JAIN <142839466+Aryanjain75@users.noreply.github.com>
2024-12-20 13:59:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const movieRouter = require("./controllers/movie"); // Renaming router to movieRouter for clarity
-const movieIdRouter = require("./controllers/mivieid"); // Renaming Router to movieIdRouter for clarity
-const authRouter = require("./controllers/Auth"); // Renaming Route to authRouter for clarity
-
+const movieRouter = require("./controllers/movie");
+const movieIdRouter = require("./controllers/mivieid");
+const authRouter = require("./controllers/Auth");
 const cookieParser = require("cookie-parser");
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// Define allowed origins
+const allowedOrigins = process.env.ORIGINS||["http://localhost:5174"];
+
 // CORS Middleware
 app.use(cors({
-  origin: "*", 
+  origin: function (origin, callback) {
+    // Allow requests with no origin (e.g., mobile apps, Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true
+  credentials: true, // Allow credentials
 }));
 
 // Middleware for parsing JSON and URL-encoded bodies
@@ -24,9 +35,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Define routes
-app.use("/", movieRouter);  // Main route for movies
-app.use("/in", movieIdRouter);  // Route for movie details
-app.use("/auth", authRouter);  // Authentication routes
+app.use("/", movieRouter);
+app.use("/in", movieIdRouter);
+app.use("/auth", authRouter);
 
 // Set the port from environment or default to 3000
 const PORT = process.env.PORT || 3000;
